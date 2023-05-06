0604127dcddabe7727a8c48030750a32011034c7
marijajovanovaa <marijajovanova03@yahoo.com>
2023-05-06 13:14:12 +0200

    EMB-29 authentication using JWT tokens

---
+++ server/middleware/auth.js
@@ -0,0 +1,21 @@
+import { jwt } from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  const authHeader = req.headers.token;
+  try {
+    let token = req.header("Authorization");
+    if (!token) {
+      return res.status(403).json("You are not authenticated!");
+    }
+
+    if (token.startsWith("Bearer ")) {
+      token = token.substring(7, token.length).trimLeft();
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
