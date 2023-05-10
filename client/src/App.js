fe4f7127f9d9ba61ad3b3ef81536e198006f2fde
Andrea <andrea.korunoska@student.um.si>
2023-05-10 17:39:21 +0200

    EMB-35 Client sockets

--- client/src/App.js
+++ client/src/App.js
@@ -4,29 +4,36 @@ import LoginPage from "views/LoginView";
 import RegisterPage from "views/RegisterView";
 import { useSelector } from "react-redux";
 import Navbar from "components/Navbar";
+import TestComponent from "components/Test";
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import  useSocket from "hooks/useSocket";
 
 
 function App() {
   const user = useSelector((state) => state.user);
   const token = useSelector((state) => state.token);
 
+  const socket = useSocket("http://localhost:3002");
+
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="pt-20">
-         {/* User ID: {user._id} */}
-        {token}
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </>
+      <>
+        <BrowserRouter>
+
+          <Navbar />
+          <div className="pt-20">
+            {/* User ID: {user._id} */}
+            {token}
+            <Routes>
+              <Route path="/" element={<LandingPage socket={socket} />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </>
   );
 }
 
--- client/src/views/LandingView.jsx
+++ client/src/views/LandingView.jsx
@@ -1,4 +1,19 @@
-const LandingPage = () => {
+import React, { useEffect } from "react";
+
+const LandingPage = ({socket}) => {
+  useEffect(() => {
+    if (socket) {
+      socket.on("notification", (data) => {
+        console.log("Received notification:", data.message);
+        // Display the notification using a library or custom code
+      });
+
+      // Cleanup when the component is unmounted
+      return () => {
+        socket.off("notification");
+      };
+    }
+  }, [socket]);
   return <>HomePage</>;
 };
 
