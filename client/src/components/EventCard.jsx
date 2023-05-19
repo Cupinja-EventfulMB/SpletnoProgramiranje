f7bd4fa06a5da6c223acc236ef0d2c40458ff6a1
Cece Pasinechka <cvetankapasinecka@gmail.com>
2023-05-19 19:57:15 +0200

    EMB-56 EventCard and EventCardContainer components

---
+++ client/src/components/EventCard.jsx
@@ -0,0 +1,30 @@
+import { BsCalendar4Week } from "react-icons/bs";
+import Button from "./Button";
+
+const EventCard = ({ event }) => {
+  return (
+    <div className="relative shadow-md rounded-2xl w-80 h-60 bg-white hover:shadow-xl transition duration-200 group">
+      <img
+        src={event.image}
+        alt=""
+        className="object-cover object-center w-full h-full aboslute rounded-2xl"
+      />
+      <div className="px-4 py-[8px] absolute w-full bottom-0 gap-2 left-0 bg-white rounded-2xl z-10 flex flex-col group-hover:bg-rose-500 group-hover:text-white transition druation-200">
+        <h3 className="font-semibold">{event.name}</h3>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
+            <BsCalendar4Week className="text-gray-400 group-hover:text-white mr-2" />
+            <p className="text-gray-400 text-sm group-hover:text-white">
+              {event.date}
+            </p>
+          </div>
+        </div>
+        <div className="absolute right-4 top-1/2 -translate-y-1/2">
+          <Button title={"Info"} outline />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventCard;
--- client/src/views/LandingView.jsx
+++ client/src/views/LandingView.jsx
@@ -2,7 +2,25 @@ import React, { useEffect } from "react";
 
 //COMPONENTS
 import Button from "../components/Button";
+import EventCardContainer from "components/EventCardContainer";
 import Gradient from "components/Gradient";
+
+const events = [
+  {
+    id: 1,
+    name: "Event 1",
+    date: "3 May 2023",
+    image:
+      "https://content.eventim.com/static/uploaded/at/p/b/h/g/pbhg_960_360.webp",
+  },
+  {
+    id: 2,
+    name: "Event 2",
+    date: "8 May 2023",
+    image:
+      "https://content.eventim.com/static/uploaded/at/x/i/x/u/xixu_960_360.webp",
+  },
+];
 
 const LandingView = ({ socket }) => {
   useEffect(() => {
@@ -21,6 +39,12 @@ const LandingView = ({ socket }) => {
   return (
     <>
       <Gradient />
+
+      <div className="container mx-auto">
+        <EventCardContainer events={events} title={"Trending"} />
+        <EventCardContainer events={events} title={"Today in Maribor"} />
+        <EventCardContainer events={events} title={"Upcoming"} />
+      </div>
     </>
   );
 };
