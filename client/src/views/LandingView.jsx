2f947f8f4945929e0d18f79e9dca5101e4e2759e
Andrea <andrea.korunoska@student.um.si>
2023-05-20 21:32:27 +0200

    EMB-57 Landing View changes

--- client/src/views/LandingView.jsx
+++ client/src/views/LandingView.jsx
@@ -1,29 +1,14 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 
 //COMPONENTS
 import Button from "../components/Button";
 import EventCardContainer from "components/EventCardContainer";
 import Gradient from "components/Gradient";
 import Search from "components/Search";
-
-const events = [
-  {
-    id: 1,
-    name: "Event 1",
-    date: "3 May 2023",
-    image:
-      "https://content.eventim.com/static/uploaded/at/p/b/h/g/pbhg_960_360.webp",
-  },
-  {
-    id: 2,
-    name: "Event 2",
-    date: "8 May 2023",
-    image:
-      "https://content.eventim.com/static/uploaded/at/x/i/x/u/xixu_960_360.webp",
-  },
-];
+import MapMaribor from "components/MapMaribor"
 
 const LandingView = ({ socket }) => {
+  const [events, setEvents] = useState([])
   useEffect(() => {
     if (socket) {
       socket.on("notification", (data) => {
@@ -37,12 +22,22 @@ const LandingView = ({ socket }) => {
       };
     }
   }, [socket]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/api/event', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => res.json()).then(body => setEvents(body))
+  }, [])
+
   return (
     <>
       <Gradient />
-    
       <div className="container mx-auto">
         <Search />
+        <MapMaribor />
         <EventCardContainer events={events} title={"Trending"} />
         <EventCardContainer events={events} title={"Today in Maribor"} />
         <EventCardContainer events={events} title={"Upcoming"} />
