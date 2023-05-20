19ee418b46bcb505ea202158836b6861294844b5
Andrea <andrea.korunoska@student.um.si>
2023-05-20 21:48:55 +0200

    EMB-61 creating a map for Maribor

--- client/src/components/MapMaribor.jsx
+++ client/src/components/MapMaribor.jsx
@@ -1,17 +1,16 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import L from 'leaflet';
 import {MapContainer, Marker, Pane, Popup, Rectangle, TileLayer} from "react-leaflet";
 import {render} from "react-dom/profiling";
 import 'leaflet/dist/leaflet.css';
 
-const MapMaribor = ({ event }) => {
+const MapMaribor = ({event}) => {
     const position = [46.5547, 15.6459]
     return (
-        <MapContainer center={position} zoom={13.5} style={{ height: "450px" }}>
+        <MapContainer center={position} zoom={13.5} style={{height: "450px"}}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
         </MapContainer>
     );
 
