40f75fddff7ca5acf91c4fef09173829bc76db65
Andrea <andrea.korunoska@student.um.si>
2023-05-20 21:54:12 +0200

    EMB-61 modifications applied on Landingview (displaying the map of Maribor)

--- client/src/views/LandingView.jsx
+++ client/src/views/LandingView.jsx
@@ -7,43 +7,43 @@ import Gradient from "components/Gradient";
 import Search from "components/Search";
 import MapMaribor from "components/MapMaribor"
 
-const LandingView = ({ socket }) => {
-  const [events, setEvents] = useState([])
-  useEffect(() => {
-    if (socket) {
-      socket.on("notification", (data) => {
-        console.log("Received notification:", data.message);
-        // Display the notification using a library or custom code
-      });
+const LandingView = ({socket}) => {
+    const [events, setEvents] = useState([])
+    useEffect(() => {
+        if (socket) {
+            socket.on("notification", (data) => {
+                console.log("Received notification:", data.message);
+                // Display the notification using a library or custom code
+            });
 
-      // Cleanup when the component is unmounted
-      return () => {
-        socket.off("notification");
-      };
-    }
-  }, [socket]);
+            // Cleanup when the component is unmounted
+            return () => {
+                socket.off("notification");
+            };
+        }
+    }, [socket]);
 
-  useEffect(() => {
-    fetch('http://localhost:3001/api/event', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(res => res.json()).then(body => setEvents(body))
-  }, [])
+    useEffect(() => {
+        fetch('http://localhost:3001/api/event', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.json()).then(body => setEvents(body))
+    }, [])
 
-  return (
-    <>
-      <Gradient />
-      <div className="container mx-auto">
-        <Search />
-        <MapMaribor />
-        <EventCardContainer events={events} title={"Trending"} />
-        <EventCardContainer events={events} title={"Today in Maribor"} />
-        <EventCardContainer events={events} title={"Upcoming"} />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Gradient/>
+            <div className="container mx-auto">
+                <Search/>
+                <MapMaribor/>
+                <EventCardContainer events={events} title={"Trending"}/>
+                <EventCardContainer events={events} title={"Today in Maribor"}/>
+                <EventCardContainer events={events} title={"Upcoming"}/>
+            </div>
+        </>
+    );
 };
 
 export default LandingView;
