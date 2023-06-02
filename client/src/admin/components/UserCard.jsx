25641604ced5cac207ae8a21cf00a26889055107
Cece Pasinechka <cvetankapasinecka@gmail.com>
2023-06-03 01:29:41 +0200

    EMB-86 Added listing users from database

---
+++ client/src/admin/components/UserCard.jsx
@@ -0,0 +1,30 @@
+import { AiFillEdit } from "react-icons/ai";
+import Button from "../../components/form/Button";
+
+const UserCard = ({ user }) => {
+  if (!user) return <div>Loading...</div>;
+  return (
+    <div className="w-full bg-white flex flex-row justify-between h-20 items-center px-8">
+      <div className="flex flex-col">
+        <p className="text-gray-500 text-sm">{user.name}</p>
+        <p className="text-gray-500 text-sm">{user.email}</p>
+      </div>
+      <div className="flex flex-col">
+        <p className="text-gray-500 text-sm">{user.phone}</p>
+        <p className="text-gray-500 text-sm">{user.date}</p>
+      </div>
+      <div className="flex flex-col">
+        <p className="text-gray-500 text-sm">{user.address}</p>
+
+        {user.admin ? <p>Admin</p> : <p>User</p>}
+      </div>
+      <div className="flex flex-col cursor-pointer">
+        <div onClick={() => console.log("click")}>
+          <AiFillEdit size={32} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
--- client/src/admin/views/AdminUserView.js
+++ client/src/admin/views/AdminUserView.js
@@ -1,12 +1,40 @@
-import AdminHero from "admin/components/AdminGradient";
+import AdminHero from "../components/AdminGradient";
+import UserCard from "admin/components/UserCard";
+import React, { useEffect, useState } from "react";
 
 const AdminUserView = () => {
-    return (
-      <>
-        <AdminHero />
-        <p>Edit or delete users</p>
-      </>
-    )
-  }
-  
-  export default AdminUserView;
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState(users);
+
+  const handleSearch = (value) => {
+    setSearch(users.filter((user) => user.name.toLowerCase().includes(value)));
+  };
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/api/user");
+        const data = await response.json();
+        setUsers(data);
+        setSearch(data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+    fetchUsers();
+  }, []);
+  if (!users) return <div>Loading...</div>;
+
+  return (
+    <>
+      <AdminHero />
+      <div className="container mx-auto pt-4">
+          {search.map((user) => (
+            <UserCard user={user} key={user._id} />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default AdminUserView;
